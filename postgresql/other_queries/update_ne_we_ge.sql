083a43b375dd4d93ba4be768cb921afb509e857c
Hamed_Sayidi <hamed.sayidi@dns-net.de>
2021-01-21 12:14:20 +0100

    otherQueries . update we/ge For  Gemeinde Barleben(15083040) in Sachsen_anhalt

--- postgresql/other_queries/update_ne_we_ge.sql
+++ postgresql/other_queries/update_ne_we_ge.sql
@@ -117,3 +117,37 @@ update adressen.adressen ad set anzahl_wohneinheit= sel.we , anzahl_gewerbeeinhe
 	
 	
 select * from  adressen.adressen where gemeinde_schluessel='15083415' and anzahl_wohneinheit is null
+
+---------------------------------------------------------------------------------------------------------------------------------
+-- For  Gemeinde Barleben(15083040) in Sachsen_anhalt
+-- first inport the shape file in adressen.we_ge 
+
+ALTER TABLE we_ge add column adresse_id uuid;
+
+
+update we_ge set adresse_id=null
+
+update we_ge set adresse_id=sel3.id_ad from 
+	(
+		SELECT * FROM 
+		(
+			select sel1.* , (select replace(  plz||replace(lower(replace(strasse, ' ', '')) , 'straße', 'str')  ||lower(hausnr::text || coalesce(hausnr_z,'') ), '.', '') from we_ge where we_ge.id=sel1.wid) schl_wege from 
+				(
+					select sel.*, (select id wid from we_ge where st_distance(we_ge.geom, sel.geom_ad,false)<20 order by st_distance(we_ge.geom, sel.geom_ad) limit 1 ) from
+						(
+						select id id_ad, plz, replace(lower(replace(strasse, ' ', '')) , 'straße', 'str')strasse_ad, hausnr||coalesce(lower(adresszusatz),'') hausnr_ad , geom geom_ad from adressen.adressen where gemeinde_schluessel='15083040'
+						) sel
+					) sel1
+		) sel2 where schl_wege= plz||strasse_ad||lower(hausnr_ad)	
+	) sel3 where sel3.wid=we_ge.id;
+
+	
+	
+select count(*) from we_ge where adresse_id is null
+
+update adressen.adressen ad set anzahl_wohneinheit= sel.we , anzahl_gewerbeeinheit=sel.ge from
+	(select * from we_ge) sel
+	where gemeinde_schluessel='15083040' and ad.id=uuid(sel.adresse_id);
+
+
+
